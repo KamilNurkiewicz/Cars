f1e34e7625511eccae91e2b33a9a700733651355
KamilNurkiewicz <you@example.comnurkiewiczkamil@gmail.com>
2021-01-27 00:48:02 +0100

    change focus on second section to scroll animation based on window height. Change html and css in fourth section

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,8 @@ const content = document.querySelector('.content');
 const mainHeader = document.querySelector('.main-header');
 const mainFooter = document.querySelector('.main-footer');
 const navItems = document.querySelector('.mobile-nav__items');
+const secondSection = document.querySelector('.second-section');
+const screenPosition = window.innerHeight / 1.5;
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('main-header__hamburger--active');
@@ -13,3 +15,15 @@ hamburger.addEventListener('click', () => {
   mainFooter.classList.toggle('main-footer--mobile-nav-active');
   navItems.classList.toggle('mobile-nav__items--active');
 });
+
+const scrollAppear = () => {
+  const secondSectionPosition = secondSection.getBoundingClientRect().top;
+
+  if (secondSectionPosition < screenPosition) {
+    secondSection.classList.add('second-section--animation');
+  } else {
+    secondSection.classList.remove('second-section--animation');
+  }
+};
+
+window.addEventListener('scroll', scrollAppear);
